05cafb98570f5d3af430eb6c1656aae5ac8bbf2f
projets-pe <projets-pe@sycorax.fr>
2020-02-24 13:28:06 +0100

    helpers angular jwt | alertifyjs

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +9,20 @@ import { map } from 'rxjs/operators';
 export class AuthService {
 
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodeToken: any;
 
   constructor(private http: HttpClient) { }
 
   login(model: any) {
-  
     return this.http.post(this.baseUrl + 'login', model)
       .pipe(
         map((response: any) => {
           const user = response;
           if (user) {
             localStorage.setItem('token', JSON.stringify(user.token));
+            this.decodeToken = this.jwtHelper.decodeToken(user.token);
+            console.log(this.decodeToken);
           }
         })
       );
@@ -26,6 +30,11 @@ export class AuthService {
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'register' , model);
+  }
+
+  loggedIin() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -12,16 +13,16 @@ export class RegisterComponent implements OnInit {
   // faire communiquer l enfants avec le parent  ex: click d un boutton via emmetteur d evenement
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('ok successfull');
+      this.alertify.success('registration successfully');
     }, error => {
-      console.log(error);
+      this.alertify.error('user all ready exist');
     });
   }
 
